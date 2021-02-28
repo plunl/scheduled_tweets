1781b547584e2c9e4c608ceae46e587bf339da85
Pavel L <plunl2012@gmail.com>
2021-02-28 16:55:00 +0200

    handling sign up errors

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,20 @@
 class RegistrationsController < ApplicationController
   def new
-    @uer = User.new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "Seccessfully created account"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true
+  validates :email, format: /\w+@\w+\.{1}[a-zA-z]{2,}/
 end
