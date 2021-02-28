6cac5d54341dd8e9dcfd7633715ed01172ba8324
Pavel L <plunl2012@gmail.com>
2021-02-28 18:11:29 +0200

    loggin with session cookies

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,7 +6,8 @@ class RegistrationsController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path, notice: "Seccessfully created account"
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Successfully created account"
     else
       render :new
     end
